4d10cc03f4092840e3795816d1a771580cdce168
riftaway7-code <riftaway7@gmail.com>
2026-08-15 22:49:27 -0700

    Redesign community with Discord-Reddit app shell

--- community/src/App.tsx
+++ community/src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
+import { Sidebar } from './components/Sidebar'
 import { Topbar } from './components/Topbar'
 import { Home } from './pages/Home'
 import { CategoryPage } from './pages/CategoryPage'
@@ -6,15 +7,18 @@ import { PostDetail } from './pages/PostDetail'
 
 export default function App() {
   return (
-    <div className="wrap">
-      <Topbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/c/:slug" element={<CategoryPage />} />
-        <Route path="/p/:postId" element={<PostDetail />} />
-      </Routes>
-      <div className="footer">
-        HackMate Community · <a href="../">back to hackmate</a>
+    <div className="app-shell">
+      <Sidebar />
+      <div className="app-main">
+        <Topbar />
+        <main className="content-area">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/c/:slug" element={<CategoryPage />} />
+            <Route path="/p/:postId" element={<PostDetail />} />
+          </Routes>
+        </main>
+        <div className="footer">HackMate Community · <a href="../">back to hackmate</a></div>
       </div>
     </div>
   )
